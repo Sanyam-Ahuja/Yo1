f19cec0e1900a72bdb1a18ed946b1973df178d30
Sanyam Ahuja <sanyam.ahuja.alwar@gmail.com>
2022-02-03 18:21:32 +0530

    ≡ƒæì≡ƒÜÇauto commit≡ƒÜÇ≡ƒÑè

--- My Projects/Yo/yo.py
+++ My Projects/Yo/yo.py
@@ -1,13 +1,15 @@
 import pyttsx3
-# import time
+import time
 t = 0
-# time.sleep(5)
-while t < 1_00_000:
+time.sleep(10)
+while t < 1_00_00000000:
     t += 1
     engin = pyttsx3.init()
     voices = engin.getProperty('voices')
     engin.setProperty('voice', voices[2].id)
-    engin.setProperty("rate", 400)
+    engin.setProperty("rate", 260)
     engin.say(t)
-    print(t)
+    engin.setProperty("rate", 240)
+    engin.say("I will Make This Program Count Until Noticed By Mrbeast")
+    print(t, "\nI will Make This Program Count Until Noticed By Mrbeast")
     engin.runAndWait()
