e2019f6739b6c067358bda06899301af119bf1d7
Sanyam Ahuja <sanyam.ahuja.alwar@gmail.com>
2022-02-03 12:51:39 +0530

    ≡ƒæì≡ƒÜÇauto commit≡ƒÜÇ≡ƒÑè

---
+++ My Projects/Yo/yo.py
@@ -0,0 +1,12 @@
+import pyttsx3
+# import time
+t = 0
+# time.sleep(5)
+while t < 1_00_000:
+    t += 1
+    engin = pyttsx3.init()
+    voices = engin.getProperty('voices')
+    engin.setProperty('voice', voices[2].id)
+    engin.setProperty("rate", 400)
+    engin.say(t)
+    engin.runAndWait()
